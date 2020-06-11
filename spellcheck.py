99e9dd3ce237d497adf5c8c0bd5d6ed119c40459
joaoperfig <joaoperfig@gmail.com>
2020-06-11 21:47:02 +0100

    add english dictionary

--- spellcheck.py
+++ spellcheck.py
@@ -17,8 +17,19 @@ def browsercontent(url):
     return mystr
 
 def priblink(word):
-    return "https://dicionario.priberam.org/"+clean(word)
+    return "https://www.dictionary.com/browse/"+clean(word)
 
 def check(word):
     web = browsercontent(priblink(word))
-    return not (("Palavra n"+chr(227)+"o encontrada.") in web) 
+    if (web == "ERROR"):
+        return False
+    return not (("No results found for") in web) 
+
+# PORTUGUESE VERSION
+#
+#def priblink(word):
+#    return "https://dicionario.priberam.org/"+clean(word)
+#
+#def check(word):
+#    web = browsercontent(priblink(word))
+#    return not (("Palavra n"+chr(227)+"o encontrada.") in web) 
